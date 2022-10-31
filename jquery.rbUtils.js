c81592fa6ff8f298d45d1760e528ebced02aec70
Riu A. Baring <riubaring@gmail.com>
2022-10-30 18:28:32 -0700

    Update jquery.rbUtils.js

--- jquery.rbUtils.js
+++ jquery.rbUtils.js
@@ -1,6 +1,8 @@
 ;(function ($) {
    $.fn.rbAutoComplete = function () {};
+   $.fn.rbCalendar = function () {} ;
    $.fn.rbMessage = function () {};
+   $.fn.rbSliders = function () {};
    $.fn.rbTiles = function () {};
    $.fn.rbToCamelCase = function () {};
    $.fn.rbToSnakeCase = function () {};
